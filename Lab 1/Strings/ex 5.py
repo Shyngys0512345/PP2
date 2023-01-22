a780b743175776c10960f5d69791bb8efb0b4ea7
Shyngys0512345 <123059650+Shyngys0512345@users.noreply.github.com>
2023-01-22 23:41:46 +0600

    commit

---
+++ Lab 1/Strings/ex 5.py
@@ -0,0 +1,9 @@
+txt = "The best things in life are free!"
+print("free" in txt)
+
+#2
+"""
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+"""
---
+++ Lab 1/Strings/ex 6.py
@@ -0,0 +1,9 @@
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+#2
+"""
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+"""
